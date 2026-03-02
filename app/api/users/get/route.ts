de0af79dcbd96b8b3e9dc844a9f0a94ac1da3721
abdulrahman-shawi <aboodi.sy91@gmail.com>
2026-03-02 13:57:13 +0300

    قق

--- app/api/users/get/route.ts
+++ app/api/users/get/route.ts
@@ -3,6 +3,8 @@ import { prisma } from "@/lib/prisma";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
     try {
         const session = cookies().get("skynova")?.value;
